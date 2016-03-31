762c4811287bfe71cf0107de428f5c5e1dc5d91a
Minju Park <psj0943@naver.com>
2016-03-31 20:59:10 +0900

    #10 add button & onClicklistener

--- app/src/main/java/kr/hs/emirim/minju0227/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/minju0227/monchicken/MainActivity.java
@@ -3,23 +3,40 @@ package kr.hs.emirim.minju0227.monchicken;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
     EditText mName;
+    Button mButton;
 
     @Override
+    // 한번만 기술 해준다.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText)findViewById(R.id.name);
+        mName = (EditText) findViewById(R.id.name);
+        mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
+        mButton.setOnClickListener(this);
 
     }
-
+    // 화면이 보여질 때마다 실행된다.
     protected void onResume() {
         super.onResume();
         mName.setText("");
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override // aanotation
+    public void onClick(View v) {
+        Toast.makeText(this, "hugry", Toast.LENGTH_LONG).show();
     }
 }
