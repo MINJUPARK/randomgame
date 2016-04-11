eeaeffec0a1bcd7ff75b7566ce958a00124560bc
Minju Park <psj0943@naver.com>
2016-04-11 21:02:21 +0900

    #8 add Splash Activity

---
+++ app/src/main/java/kr/hs/emirim/minju0227/monchicken/SplashActivity.java
@@ -0,0 +1,37 @@
+package kr.hs.emirim.minju0227.monchicken;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        boolean b = handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // 내가 사용하고 있는 문맥객체 반환
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+
+                // 현재 스플레쉬 액티비티를 소멸시킴
+                finish();
+            }
+        }, 3000);
+
+    }
+
+
+}
